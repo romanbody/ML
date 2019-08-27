4863036103c66d1495f2d739bb99e13c3d61b6be
Roman Body <rbo@gratex.com>
2019-08-27 15:48:28 +0200

    test1

--- demo/case2/test1.py
+++ demo/case2/test1.py
@@ -1,2 +1,13 @@
-print (1)
+class rbo():
+    first_name = ""
 
+    def __init__(self,init_name):
+        print ('init1')
+        self.first_name = init_name
+
+    def print_out (self,o1):
+        for x in range(1,o1,3):
+            print (self.first_name + str(x))
+
+rbo1 = rbo('roman')
+rbo1.print_out(10)
